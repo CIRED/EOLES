677fc31b6d2708fbb3672bcdb40b92c3ce81b793
HippolyteCediey <hippolyte.cediey@gmail.com>
2024-07-30 10:03:11 +0200

    Fixed example.py and added python aux files to .gitignore

--- example.py
+++ example.py
@@ -4,10 +4,10 @@ from modelEoles import ModelEOLES
 
 example = ModelEOLES(name="example_2006_norsv", config=get_config(path="config/config_2006.json"), output_path="outputs", include_reserve=False)
 
-eval.build_model()
-eval.solve(solver_name="gurobi")
-eval.extract_optimisation_results()
+example.build_model()
+example.solve(solver_name="gurobi")
+example.extract_optimisation_results()
 
-eval.summary.to_csv("outputs/example_2006_norsv/summary.csv")
-eval.results.to_csv("outputs/example_2006_norsv/results.csv")
-eval.hourly_generation.to_csv("outputs/example_2006_norsv/hourly_generation.csv")
+example.summary.to_csv("outputs/summary.csv")
+example.results.to_csv("outputs/results.csv")
+example.hourly_balance.to_csv("outputs/hourly_generation.csv")
